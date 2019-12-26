5c5edd31d04295861715cb4b7fa7eb8a009e21dc
Ivan <ivankoy@gmail.com>
2019-12-26 19:03:04 +0100

    Designing UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using pingak9;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    Text debugLogger;
+    public void DebugLog(string log)
+    {
+        debugLogger.text = log;
+        Debug.Log(log);
+    }
+
+    public void OnDatePicker()
+    {
+        var today = DateTime.Today;
+        DebugLog("Started date picking");
+        NativeDialog.OpenDatePicker(today.Year, today.Month, today.Day,
+            (DateTime _date) =>
+            {
+                DebugLog(_date.ToString());
+            },
+            (DateTime _date) =>
+            {
+                DebugLog(_date.ToString());
+            });
+    }
+}
