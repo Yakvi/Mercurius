d8e327008b3b3c8eb20ee29fb47a53a689db1c2b
Ivan <ivankoy@gmail.com>
2019-12-26 17:30:05 +0100

    First Commit

---
+++ Assets/Scripts/DatePicker/NativeDialog.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace pingak9
+{
+    public class NativeDialog
+    {
+        public NativeDialog() { }
+
+        public static void OpenDatePicker(int year , int month, int day, Action<DateTime> onChange = null, Action<DateTime> onClose = null)
+        {
+            MobileDateTimePicker.CreateDate(year, month, day, onChange, onClose);
+        }
+        public static void OpenTimePicker(Action<DateTime> onChange = null, Action<DateTime> onClose = null)
+        {
+            MobileDateTimePicker.CreateTime(onChange, onClose);
+        }
+    }
+}
---
+++ Assets/Scripts/PopupView.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using pingak9;
+using UnityEngine.UI;
+using System;
+using System.Globalization;
+
+public class PopupView : MonoBehaviour
+{
+    public Text txtLog;
+    public void DebugLog(string log)
+    {
+        txtLog.text = log;
+        Debug.Log(log);
+    }
+
+    public void OnDatePicker()
+    {
+        var today = DateTime.Today;
+        NativeDialog.OpenDatePicker(today.Year, today.Month, today.Day,
+            (DateTime _date) =>
+            {
+                DebugLog(_date.ToString());
+            },
+            (DateTime _date) =>
+            {
+                DebugLog(_date.ToString());
+            });
+    }
+    public void OnTimePicker()
+    {
+        NativeDialog.OpenTimePicker(
+            (DateTime _date) =>
+            {
+                DebugLog(_date.ToString());
+            },
+            (DateTime _date) =>
+            {
+                DebugLog(_date.ToString());
+            });
+    }
+}
